cc5b0c96884b25f6d13f7baa02efd89a21ab490f
fabiogarcia-dev <fabio.ti.am@gmail.com>
2025-09-22 18:53:23 -0400

    banco de dados em criação para um sistema de controle financeiro

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import app from './app'
+import dotenv from 'dotenv'
+import { prismaConnect } from './config/prisma';
+import { initializeGlobalCategories } from './services/globalCategories.service';
+dotenv.config();
+
+const PORT = Number(process.env.PORT);
+
+const startServer = async() => {
+    try{
+
+        await prismaConnect();
+
+        await initializeGlobalCategories();
+
+        await app.listen({port: PORT}).then(() => { console.log(`Servidor rodando na porta ${PORT}`)});
+    
+    }catch(err){
+        console.error(err)
+    }
+}
+
+startServer()
